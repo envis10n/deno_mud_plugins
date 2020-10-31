4bebdf7b08a55a8a3f12b68cad736d3723026290
Envis10n <envis10n@protonmail.com>
2020-10-31 16:28:29 -0500

    Initial push

---
+++ deps.ts
@@ -0,0 +1,3 @@
+export { TcpClient, TcpServer } from "https://raw.githubusercontent.com/envis10n/deno_mud/main/src/net/tcp.ts";
+export { buildGMCP, parseGMCPSupports, Option } from "https://raw.githubusercontent.com/envis10n/deno_mud/main/src/net/telnet.ts";
+export { env } from "https://raw.githubusercontent.com/envis10n/deno_mud/main/src/config.ts";
---
+++ gmcp.ts
@@ -0,0 +1,34 @@
+import { TcpClient, TcpServer } from "./deps.ts";
+import { buildGMCP, parseGMCPSupports, Option } from "./deps.ts";
+import { env } from "./deps.ts";
+
+function log(...args: any[]): void {
+  console.log("[GMCP]", ...args);
+}
+
+export const __id = "gmcp";
+export async function __init(context: { [key: string]: any; }) {
+  context.server.addListener("connect", async function (this: TcpServer, client: TcpClient) {
+    client.parser.options.support(Option.GMCP);
+    client.parser.WILL(Option.GMCP);
+  });
+  context.server.addListener("gmcp", async function (this: TcpClient, namespace: string, data: string | string[] | { [key: string]: any }) {
+    switch (namespace) {
+      case "Core.Hello":
+        data = <{ [key: string]: any }>(data);
+        const client: string = data.client || data.Client || "[NO CLIENT]";
+        const version: string = data.version || data.Version || "[NO VERSION]";
+        log(`<${this.guid}> GMCP enabled. Hello from ${client} ${version}`);
+        break;
+      case "Core.Supports.Set":
+        log(parseGMCPSupports(data as string[]));
+        break;
+      case "External.Discord.Hello":
+        this.send(buildGMCP("External.Discord.Info", { applicationid: env["DISCORD_APPID"] }));
+        break;
+      default:
+        log(`<${this.guid}> GMCP Event ${namespace}`, data);
+        break;
+    }
+  });
+}
