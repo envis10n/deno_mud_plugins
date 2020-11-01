048d789762797d592fa5fa9b82620fbb9e512dc0
Envis10n <envis10n@protonmail.com>
2020-10-31 20:26:36 -0500

    Storage demo

--- storage.ts
+++ storage.ts
@@ -1,6 +1,6 @@
 import * as api from "https://raw.githubusercontent.com/envis10n/deno_mud/main/src/plugin_api.ts";
 
-interface IStorageDemo {
+interface IStorageDemo extends api.IDocument {
   test: string;
 }
 
@@ -8,6 +8,7 @@ export const __id = "storage";
 
 export async function __init(context: api.IContext): Promise<void> {
   const col: api.Collection<IStorageDemo> = context.db.collection("storage-demo");
-  col.contents.push({ test: "Hello, world!" });
+  col.insert({ test: "Hello, world!" });
+  col.insert({ test: "There can only be one!", _key: "demo1" });
   console.log(col.contents);
 }
